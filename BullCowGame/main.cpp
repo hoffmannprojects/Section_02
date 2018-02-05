5afaa01d7e436d71197a5ce6e1eb238cefe47b82
Tim <tim@hoffmannprojects.com>
2018-02-05 20:55:21 +0100

    Lecture 19: Variables and cin for input

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,17 +1,30 @@
-// Includes the standard library "iostream".
-#include <iostream>
+// Logical comments in their own line. // C++ help annotations inline.
 
-// Using the "std" namespace, so "cout" can be used instead of "std::cout".
-using namespace std;
+#include <iostream> // Includes the standard library "iostream".
+#include <string> // Includes standard library for strin operations, eg. >>.
+
+using namespace std; // Using the "std" namespace, so "cout" can be used instead of "std::cout".
 
 int main()
 {
-	// constexpr won't change at runtime.
-	constexpr int WORD_LENGT = 5;
-
-	// st::endl or "\n" for new line.
-	cout << "Welcome to Bulls and Cows. \n";
+	constexpr int WORD_LENGT = 5; // constexpr won't change at runtime.
+	
+	cout << "WELCOME TO BULLS AND COWS! \n"; // st::endl or "\n" for new line.
 	cout << "Can you guess the " << WORD_LENGT << " letter isogram I'm thinking of? \n";
 	cout << endl;
+
+	// Get a guess from the player.
+	// Store input after hitting enter.
+	cout << "Enter your guess: ";
+	
+	string Guess = "";
+	cin >> Guess;
+
+	// Repeat the guess back to them.
+	cout << "Your guess was: " << Guess;
+	cout << endl;
+	cout << endl;
+
+
 	return 0;
 }
